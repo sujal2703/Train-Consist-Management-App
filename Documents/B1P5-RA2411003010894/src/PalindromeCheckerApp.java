8a67ef8b76b39267556bc0c9a15237eaa5a52d0f
gana16priya-png <gana16priya@gmail.com>
2026-03-05 09:58:29 +0530

    UC1: Add welcome message module

---
+++ Documents/B1P5-RA2411003010894/src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // UC1: Welcome Message
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome Checker ");
+        System.out.println("=================================");
+        System.out.println("This application checks whether a word is a palindrome.");
+        System.out.println();
+
+    }
+}
